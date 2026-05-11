1ed150825addab91d624480c0fcdc9e63fee7dee
wojtekojtek <49075395+wojtekojtek@users.noreply.github.com>
2026-05-11 10:34:41 +0200

    Update 16.sh

--- a52q/voltage/16.sh
+++ a52q/voltage/16.sh
@@ -4,7 +4,7 @@ set -e
 WORK_DIR="/tmp/src/android"
 MK="device/samsung/a52q/voltage_a52q.mk"
 
-echo "debug - cleanup"
+echo "debug - cleanup $(pwd)"
 rm -rf device/samsung/a52q device/samsung/sm7125-common kernel/samsung/sm7125 vendor/samsung/sm7125-common vendor/samsung/a52q hardware/samsung-ext/interfaces
 
 repo init -u https://github.com/VoltageOS/manifest.git -b 16.2 --git-lfs --no-clone-bundle
@@ -12,27 +12,28 @@ echo "debug - syncing"
 /opt/crave/resync.sh
 
 if [ ! -d tmp ]; then
-    echo "debug - creating nfc backup"
+    echo "debug - creating nfc backup $(pwd)"
     mkdir tmp
     mv hardware/samsung/* tmp/
 fi
 
-echo "debug - trees"
+echo "debug - trees $(pwd)"
 git clone https://github.com/crdroidandroid/android_device_samsung_a52q device/samsung/a52q
 git clone https://github.com/crdroidandroid/android_device_samsung_sm7125-common device/samsung/sm7125-common
 git clone https://github.com/crdroidandroid/android_kernel_samsung_sm7125 kernel/samsung/sm7125
 git clone https://github.com/crdroidandroid/proprietary_vendor_samsung_sm7125-common vendor/samsung/sm7125-common
 git clone https://github.com/crdroidandroid/proprietary_vendor_samsung_a52q vendor/samsung/a52q
 if [ -e "hardware/samsung/ ]; then
+  echo "debug - cloning hardware samsung $(pwd)"
   git clone https://github.com/crdroidandroid/android_hardware_samsung hardware/samsung
 fi
 git clone https://github.com/crdroidandroid/hardware_samsung-extra_interfaces hardware/samsung-ext/interfaces
 
-echo "debug - restore backup"
+echo "debug - restore backup $(pwd)"
 mv tmp/* hardware/samsung/
 rm -r tmp
 
-echo "debug - patching device tree"
+echo "debug - patching device tree $(pwd)"
 sed -i 's/lineage/voltage/g' device/samsung/a52q/AndroidProducts.mk
 mv device/samsung/a52q/lineage_a52q.mk "$MK"
 sed -i 's/lineage/voltage/g' "$MK"
@@ -43,7 +44,7 @@ grep -q 'TARGET_ENABLE_BLUR'           "$MK" || echo 'TARGET_ENABLE_BLUR := true
 grep -q 'EXTRA_UDFPS_ANIMATIONS'       "$MK" || echo 'EXTRA_UDFPS_ANIMATIONS := true'         >> "$MK"
 grep -q 'TORCH_STR_SUPPORTED'          "$MK" || echo 'TORCH_STR_SUPPORTED := true'            >> "$MK"
 
-echo "debug - keys"
+echo "debug - keys $(pwd)"
 rm -rf vendor/voltage-priv/keys
 echo 1
 git clone https://github.com/VoltageOS/vendor_voltage-priv_keys vendor/voltage-priv/keys
@@ -58,7 +59,7 @@ echo 5
 touch .keys_generated
 echo 6
 
-echo "debug - apply tee workaround"
+echo "debug - apply tee workaround $(pwd)"
 echo 7
 mkdir -p system/sepolicy/private
 echo 8
@@ -67,13 +68,13 @@ grep -q 'allow tee gatekeeper_vendor_data_file:dir' system/sepolicy/private/tee.
     >> system/sepolicy/private/tee.te
 echo 9
 
-echo "debug - add maintainer string"
+echo "debug - add maintainer string $(pwd)"
 OVERLAY_DIR="device/samsung/a52q/overlay-voltage/packages/apps/Settings/res/values"
 mkdir -p "$OVERLAY_DIR"
 [ -f "$OVERLAY_DIR/voltage_strings.xml" ] || printf '<?xml version="1.0" encoding="utf-8"?>\n<resources xmlns:xliff="urn:oasis:names:tc:xliff:document:1.2">\n    <string name="voltage_maintainer">wojtekojtek</string>\n</resources>\n' \
     > "$OVERLAY_DIR/voltage_strings.xml"
 
-echo "debug - build"
+echo "debug - build $(pwd)"
 source build/envsetup.sh
 croot
 brunch a52q
